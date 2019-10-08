81ebe142d4c114ddaca1461891982fb28a602433
馬場園翔太 <>
2019-10-08 18:00:00 +0900

    物理削除から論理削除に変更

--- app/Todo.php
+++ app/Todo.php
@@ -3,13 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+  use SoftDeletes;
+
+
+
     protected $fillable = [
       'title',
       'user_id'
     ];
+
+
 
     public function getByUserId($id)
     {
